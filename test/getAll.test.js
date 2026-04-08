09e5c1a5e4c1ccc83b43c83395e1b9deb081500f
A R <rash0093@algonquinlive.com>
2026-04-08 19:08:44 -0400

    add getAll test

---
+++ test/getAll.test.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch');
+
+const STUDENT_EMAIL = 'rash0093@algonquinlive.com';
+const BASE_URL = 'https://arteamproject-ecomdb.onrender.com';
+
+async function testGetAll() {
+    const testName = 'getAll to show all products';
+  try {
+    const res = await fetch(`${BASE_URL}/products`);
+    const status = res.status;
+    const result = status === 200 ? 'PASSED' : 'FAILED';
+    console.log(`${STUDENT_EMAIL} - ${testName} - ${status} - ${result}`);
+  } catch (err) {
+    console.log(`${STUDENT_EMAIL} - ${testName} - ERROR - FAILED`);
+  }
+}
+
+testGetAll();
