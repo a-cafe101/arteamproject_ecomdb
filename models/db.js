f3a52194a34e5689850d2a85f3a6a9e4dd3c8f30
A R <rash0093@algonquinlive.com>
2026-04-08 18:29:16 -0400

    remove atlas credentials from code

--- models/db.js
+++ models/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
  
 // Use the Atlas URI stored in .env or defaults to local db
-const dbURI = process.env.DB_URI || 'mongodb+srv://NakMuay1:nakmuay26@cluster0.bmakp11.mongodb.net/?appName=Cluster0';
- 
+const dbURI = process.env.DB_URI || 'mongodb+srv://NakMuay1:nakmuay26@cluster0.bmakp11.mongodb.net/ecommerceDB?appName=Cluster0';
 // Opens the connection
 mongoose.connect(dbURI);
  
